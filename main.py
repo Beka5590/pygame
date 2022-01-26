73cef6c6fc459839fc9e90d80b176447c8ff0101
Beka5590 <96657527+Beka5590@users.noreply.github.com>
2022-01-27 02:31:24 +0600

    Add files via upload
    добавили кнопку "Играть", окно с надписью "игра закончена", подключили фоновую музыку
    Малика, Бекарыс

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # import pygame.display
+import select
+
+import pygame.transform
 
 from modules import *
 from player import Player
@@ -9,27 +12,68 @@ pygame.init()
 
 class Main:
     def __init__(self):
+        # self.MAINSCREEN = pygame.display.set_mode((640, 480))
         self.SCREEN = pygame.display.set_mode((640, 480))
+        self.play_button = pygame.image.load('data/play_button.png')
         self.x_pos, self.y_pos = 0, 0
         self.weigh, self.height = 20, 20
         self.clock = pygame.time.Clock()
         self.player = Player()
         self.right_move = False
         self.left_move = True
-        self.coin_image_path = os.path.join('data', 'money.jpg')
-        self.coin_image = pygame.image.load(self.coin_image_path)
-        self.coin_image = pygame.transform.scale(self.coin_image, (100, 100))
-        self.fall_sound_path = os.path.join('data', 'multyashnyiy-zvuk-padeniya.mp3')
-        self.fall_sound = pygame.mixer.Sound(self.fall_sound_path)
+        self.pizza_image = pygame.image.load('data/pizzza.png').convert()
+        self.pizza_image = pygame.transform.scale(self.pizza_image, (100, 100))
+        self.fon_music = pygame.mixer.Sound('data/muzika_na_zadnij_fon_-_bez_nazvanija_(z2.fm).mp3')
+        self.fall_sound = pygame.mixer.Sound('data/multyashnyiy-zvuk-padeniya.mp3')
+        self.start()
+
+    def start(self):
+        self.fon_music.play()
+        self.SCREEN.fill((255, 255, 255))
+        self.SCREEN.blit(self.play_button, (180, 292))
+        run = True
+        while run:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    run = False
+                if i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
+                    print(i.pos, self.play_button.get_size())
+                    if i.pos[0] >= 214 and i.pos[0] <= 430 and i.pos[1] >= 335 and i.pos[1] <= 420:
+                        run = False
+            pygame.time.delay(12)
+            pygame.display.update()
+            # pygame.display.flip()
+        self.fon_music.stop()
         self.create()
         self.move()
+
+    def game_over(self):
+        self.fon_music.play()
+        # self.fail_music = pygame.mixer.Sound('data/fail_music.mp3')
+        # self.fail_music.play()
+        run = True
+        self.SCREEN.fill((0, 0, 0))
+        over_photo = pygame.image.load('data/game_over.png')
+        self.SCREEN.blit(over_photo, (120, 150))
+        retry_photo = pygame.image.load('data/retry_ar.png')
+        self.SCREEN.blit(retry_photo, (0, 0))
+        while run:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                if i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
+                    if i.pos[0] >= 2 and i.pos[0] <= 150 and i.pos[1] >= 1 and i.pos[1] <= 80:
+                        self.fon_music.stop()
+                        self.create()
+                        self.move()
+            pygame.display.flip()
 
     def create(self):  # создание монет
         self.SCREEN.fill((255, 255, 255))
         for i in range(5):
-            self.SCREEN.blit(self.coin_image, (random.randint(20, 480), random.randint(10, 480)))
+            self.SCREEN.blit(self.pizza_image, (random.randint(20, 480), random.randint(10, 480)))
         pygame.display.update()
-        pygame.display.flip()
 
     def move(self):  # движение
         moving = True
@@ -37,6 +81,7 @@ class Main:
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
                     moving = False
+                    pygame.quit()
                 if i.type == pygame.MOUSEBUTTONDOWN:
                     if i.button == 1 and self.left_move is True:
                         self.y_pos -= 50
@@ -45,7 +90,9 @@ class Main:
                         self.y_pos -= 50
                         self.x_pos -= 25
             if self.y_pos == 390:
-                print('game over!')
+                self.x_pos = 0
+                self.y_pos = 0
+                self.game_over()
             else:
                 if self.x_pos <= 540 and self.right_move is False:
                     if self.x_pos == 540:
@@ -69,8 +116,10 @@ class Main:
                     self.y_pos += 2
                     self.right_move = True
                     self.left_move = False
+            self.SCREEN.fill((255, 255, 255))
             self.SCREEN.blit(self.player.sprite.image, (self.x_pos, self.y_pos))
-            self.clock.tick(60) / 1000.0
+            # self.clock.tick(60) / 1000.0
+            pygame.time.delay(12)
             pygame.display.update()
             pygame.display.flip()
 
--- player.py
+++ player.py
@@ -7,8 +7,8 @@ class Player:
         self.position = None
         self.sprite = pygame.sprite.Sprite()
         self.all_sprites = pygame.sprite.Group()
-        filename = os.path.join('data', 'hero.jpg')
-        self.sprite.image = pygame.image.load(filename)
+        # filename = os.path.join('data', 'hero.jpg')
+        self.sprite.image = pygame.image.load('data/cool_man.png')
         self.sprite.image = pygame.transform.scale(self.sprite.image, (100, 100))
         self.sprite.rect = self.sprite.image.get_rect()
         self.all_sprites.add(self.sprite)
